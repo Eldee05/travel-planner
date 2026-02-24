ffd173725c46deb43f81b68466817b5bd6fd6037
Eldee05 <eldivinesolace@gmail.com>
2026-02-24 01:46:11 +0100

    update on travel planner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 export default App;
 
+const initialItems = [
+  { id: 1, description: "Passports ", quantity: 2, packed: false },
+  { id: 2, description: "Socks ", quantity: 12, packed: true },
+  { id: 3, description: "Tickets ", quantity: 2, packed: false },
+  { id: 4, description: "charger ", quantity: 1, packed: true },
+];
+
 function App() {
   return (
     <div className="App">
@@ -12,19 +19,46 @@ function App() {
 }
 
 function Header() {
-  return <h1> 🌴 Staycation 🧳 </h1>;
+  return <h1>🏖️ Staycation 🧳 </h1>;
 }
 
 function Form() {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3>What do you need for you trip ✈️</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}></option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+    </form>
   );
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      {" "}
+      <span style={item.packed ? { textDecoration: "Line-through" } : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
